92d9c09d4c2eb2e8143aaf3c4cf38ad51641c8fc
John Wu <johnwu93@gmail.com>
2018-01-17 21:42:35 -0800

    Finished Compose exercise

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -3,33 +3,25 @@
 
 module Course.Compose where
 
+import Course.Applicative
 import Course.Core
 import Course.Functor
-import Course.Applicative
 import Course.Monad
 
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
-
 newtype Compose f g a =
   Compose (f (g a))
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  f_a_b <$> (Compose app1_app2_value) = Compose ((f_a_b <$>) <$> app1_app2_value)
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure = Compose . pure . pure
+  (Compose app1_app2_fun) <*> (Compose app1_app2_value) = Compose ((<*>) <$$> app1_app2_fun <*> app1_app2_value)
+
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
-
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+instance (Monad f, Monad g) => Monad (Compose f g) where
+  (=<<) = error "Impossible"
